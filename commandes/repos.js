b40bf209e1d7626fa763f4cd7dee68ae41bd03cf
Marisel <shenapam3@gmail.com>
2024-10-15 14:25:33 +0300

    LEORNARD MD

--- commandes/repos.js
+++ commandes/repos.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "📑", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/betingrich/Tesla';
-  const img = 'https://telegra.ph/file/db890d1a98e3ab7ef631a.jpg';
+  const githubRepo = 'https://api.github.com/repos/Sostenestech/LEORNARD-MD';
+  const img = 'https://files.catbox.moe/ps8cqh.jpg';
 
   try {
     const response = await fetch(githubRepo);
@@ -22,13 +22,13 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "📑", nomFichier: __
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
       const gitdata = `*Hi,I am* *Leonard-Md.*\n  
-🏎️ *Pair Code* https://bugatti-session.onrender.com
-🏎️ *Repo:* ${data.html_url}
-🏎️ *Stars:* ${repoInfo.stars}
-🏎️ *𝗙𝗼𝗿𝗸𝘀:* ${repoInfo.forks}
-🏎️ *Released Date:* ${releaseDate}
-🏎️ *Updated on:* ${repoInfo.lastUpdate}
-🏎️ *Owner:* *Leonard*
+ *Pair Code* https://Leonard-session.onrender.com/
+ *Repo:* ${data.html_url}
+ *Stars:* ${repoInfo.stars}
+ *Forks:* ${repoInfo.forks}
+ *Released Date:* ${releaseDate}
+ *Updated on:* ${repoInfo.lastUpdate}
+ *Owner:* *Leonard*
 __________________________________
             *Leornard-Md*`;
 
